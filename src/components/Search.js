467626d98943a1f81eeb093c08ea3b78bea028f5
Hyrondelle <n.hyr@free.fr>
2022-03-29 21:56:42 +0200

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,15 +4,17 @@ import Card from './Card';
 
 const Search = () => {
     const [moviesData,setMoviesData] = useState([]);
+    const [searchData,setSearchData] = useState('potter');
     useEffect(()=>{
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=0a0ffe11da160d3d8acbe09b6f302b21&query=avengers&language=fr-FR')
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=0a0ffe11da160d3d8acbe09b6f302b21&query='+searchData+'s&language=fr-FR')
         .then((data)=>setMoviesData(data.data.results))}
-    ,[]);
+    ,[searchData]);
 
     return (
         <div className='search'>
             <div className='search-options'>
-                <input type='text' placeholder='choississez un film'></input>
+                <input type='text' placeholder='choississez un film'
+                onChange={(e)=>setSearchData(e.target.value)}></input>
                 <div className='btn-topflop'>
                     <button className='topToFlop'>Top<span>➞</span></button>
                     <button className='flopToTop'>Flop<span>➞</span></button>
