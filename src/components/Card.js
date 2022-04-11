9d7f545abe705e12df3a37d7df54f88475efed17
Hyrondelle <n.hyr@free.fr>
2022-04-12 00:21:16 +0200

    favoris

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,16 @@ import React from 'react';
 const image = require('../asset/liregenerique.png');
 
 const Card = (props) => {
+    const addToBest = ()=>{
+        if(!localStorage.getItem(props.movie.id))
+        localStorage.setItem(props.movie.id,props.movie.id)
+    }
+
     return (
         <div className='card'>
             <div className='card-container'>
-                <img src={props.movie.backdrop_path?"https://image.tmdb.org/t/p/original"+props.movie.backdrop_path:image} alt='movie' className='image'></img>
+                <img src={props.movie.backdrop_path?
+                    "https://image.tmdb.org/t/p/original"+props.movie.backdrop_path:image} alt='movie' className='image'></img>
                 <div className='movie-options'>
                     <h3>{props.movie.title}</h3>
                     <div className='date-vote'>
@@ -13,7 +19,12 @@ const Card = (props) => {
                         <p className='vote'><span>★</span>{props.movie.vote_average}</p>
                     </div>
                     <p className='description'>{props.movie.overview}</p>
-                    <input type='button' value="ajoutez aux favoris"></input>
+                    <input 
+                        onClick={addToBest}
+                        type='button'
+                        value="ajoutez aux favoris">
+
+                    </input>
                 </div>
             </div>
         </div>
--- src/pages/Best.js
+++ src/pages/Best.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../components/Navigation';
+import Card from '../components/Card';
 
 const Best = () => {
+    
+    const [listData,setListData] = useState([]);
+    let nbMoviesId = [];
+        if(localStorage.length!==0){     
+            for(let i = 0; i < localStorage.length; i++){
+                nbMoviesId.push(localStorage.key(i))    
+            } 
+            
+        } 
+    let j =0;
+    const tabId=[];
+    useEffect(()=>{ 
+        for(j=0;j<nbMoviesId.length;j++){
+            axios.get('https://api.themoviedb.org/3/movie/'+nbMoviesId[j]+'?api_key=0a0ffe11da160d3d8acbe09b6f302b21&language=fr-FR')
+        .then((res) =>setListData((listData)=>[...listData,res.data]))
+        }
+        console.log(listData);
+    },[j])  
+        
+
     return (
         <div className='best'>
             <Navigation/>
+            <h2>Coups de coeur ❤️</h2>
+            <div className='best-movies'>
+                {localStorage.length>0?(
+                    listData.map((movie)=><Card key={movie.id} movie={movie}/>)
+                ):
+                  (<div className='noMovies'>vous n'avez pas encore ajouté de film</div>)
+            }
+            </div>
         </div>
     );
 };
